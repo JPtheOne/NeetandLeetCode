7ef5ae8e13b0cdefe8ba6805549e5d07f8f9c04e
JPtheOne <juanpimod@gmail.com>
2023-08-04 21:34:51 -0600

    49. Solved on 04/08/23

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,7 +1,18 @@
-strs = ["eat","tea","tan","ate","nat","bat"]
-letters = []
-for i in strs:
-    new = tuple(sorted(i))
-    letters.append(new)
-uniques = set(letters)
-print(uniques)
+def groupAnagrams(strs):
+    anagrams = {}
+
+    for word in strs:
+        occurences = []
+        for letter in word:
+            amount = word.count(letter)
+            occurence = (letter,amount)
+            occurences.append(occurence)
+
+        key = tuple(sorted(occurences))
+
+        if key not in anagrams:
+            anagrams[key] = []
+
+        anagrams[key].append(word)
+
+    return anagrams.values()
