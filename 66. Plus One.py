e7ab0b73eec6ef161af0d0038fe1f04cb3974b10
JPtheOne <juanpimod@gmail.com>
2023-10-30 21:07:59 -0600

    PlusOne problem solved on 30/10

---
+++ 66. Plus One.py
@@ -0,0 +1,9 @@
+#Given an int in list, return int +1 
+#Space and Time complexity: O(n)
+def plusOne(digits):
+    s = [str(integer) for integer in digits]
+    a_string = "".join(s)
+    res = int(a_string)+1
+    answer = [int(i) for i in str(res)]
+    return answer
+
